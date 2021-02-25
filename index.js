029a52ed65b35ba0f8669567266e9b91df920d00
Heikki Mäkelä <heikki.makela@mediasignal.fi>
2021-02-25 13:31:21 +0200

    lab 4

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const port = process.env.PORT || 8080;
-app.use(express.static("public"))
+app.use(express.static("public"));
 
+let database = [
+  { id: 1, latitude: 60, longitude: 70 },
+  { id: 2, latitude: 40, longitude: 80 },
+];
+
+app.get("/api/locations", function (req, res) {
+  app.set("json spaces", "\t");
+
+  res.send(database);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

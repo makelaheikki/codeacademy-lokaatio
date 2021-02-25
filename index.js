dc968b0cf0213ed1b5751202cd405e7c22695765
Heikki Mäkelä <heikki.makela@mediasignal.fi>
2021-02-25 11:16:56 +0200

    step 2 fix

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
 
+const port = process.env.PORT || 8080;
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)

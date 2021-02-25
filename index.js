6d6e62756b4504866dd492a3e080e7f8f0394bb8
Heikki Mäkelä <heikki.makela@mediasignal.fi>
2021-02-25 13:47:48 +0200

    lab5

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-
 const port = process.env.PORT || 8080;
 app.use(express.static("public"));
 
@@ -9,10 +8,31 @@ let database = [
   { id: 2, latitude: 40, longitude: 80 },
 ];
 
-app.get("/api/locations", function (req, res) {
+var locationsrouter = express.Router();
+
+locationsrouter.get("/1", (req, res) => {
+  res.send(database[0]);
+});
+
+locationsrouter.get("/", function (req, res) {
   app.set("json spaces", "\t");
 
   res.send(database);
+});
+
+locationsrouter.get("/ugly", function (req, res) {
+  res.send(database);
+});
+
+app.use("/api/locations", locationsrouter);
+
+app.use("/hello", (req, res, next) => {
+  console.log("hello");
+  next();
+});
+app.use("/world", (req, res, next) => {
+  console.log("world");
+  next();
 });
 
 app.listen(port, () => {
